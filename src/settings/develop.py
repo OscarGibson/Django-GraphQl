20122eff3a855857bae88807c9d8b8f490a49d09
Ostap Hyba <oneostap@gmail.com>
2018-03-01 17:39:53 +0200

    init commit

---
+++ src/settings/develop.py
@@ -0,0 +1,29 @@
+from .base import *
+
+DEBUG = True
+
+ALLOWED_HOSTS = ['localhost']
+
+# Database
+# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'operationCwal0',
+        'HOST': '127.0.0.1',
+        'PORT': '5433',
+    }
+}
+
+TIME_ZONE = 'UTC'
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/2.0/howto/static-files/
+
+MEDIA_ROOT = 'media'
+MEDIA_URL = 'media/'
+STATIC_ROOT = 'static'
+STATIC_URL = 'static/'
